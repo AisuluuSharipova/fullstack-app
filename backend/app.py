c973844500b4f86c818f8c50d0d99181566a3d72
AisuluuSharipova <zhoodarbekovaaisuluu@gmail.com>
2026-04-23 08:09:35 +0500

    fixed

--- backend/app.py
+++ backend/app.py
@@ -161,7 +161,8 @@ def health():
 if __name__ == '__main__':
     init_db()
     port = int(os.environ.get("PORT", 5000))
-    app.run(host='0.0.0.0', port=port)from flask import Flask, request, jsonify
+    app.run(host='0.0.0.0', port=port)
+    from flask import Flask, request, jsonify
 from flask_cors import CORS
 import psycopg2
 import os
